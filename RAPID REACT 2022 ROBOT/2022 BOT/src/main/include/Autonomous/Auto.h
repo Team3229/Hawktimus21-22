ae7618aca4369a6a6491054424f72969e253ec22
Sebastian <43454754+SebastianRPrime@users.noreply.github.com>
2022-01-16 09:56:31 -0500

    Robot Strucutre Phased In
    File Tree Ready For Development

---
+++ RAPID REACT 2022 ROBOT/2022 BOT/src/main/include/Autonomous/Auto.h
@@ -0,0 +1,50 @@
+#ifndef AUTO_H
+#define AUTO_H
+
+// includes
+#include <iostream>
+#include <string>
+
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/XboxController.h>
+#include <Math.h>
+#include "frc/Timer.h"
+
+#include "DriveTrain/DriveSystem.h"
+#include "CaptureFile.h"
+#include "Debug.h"
+#include "ControllerInputs.h"
+
+class Auto {
+private:
+  bool autoDone = false;
+
+  // Pass in our subsystems
+  // MAKE SURE TO UPDATE CONSTRUCTOR ARGS AND SUBSYSTEMS FOR THIS YEAR!!!!!!!
+  DriveSystem * autoChassis;
+  
+
+  // files stuff
+  // Use .aut file extension
+  std::string stationFileList = "crossLineAndShoot.aut, crossLine.aut";
+  std::string defaultFileName = "defaultAutoPath.aut";
+  std::string stationText = "Auto file path: /home/lvuser/";
+  std::string inputFileName = "crossLineAndShoot"; //other was crossLine
+
+  const bool WRITE = true;
+  const bool READ = false;
+  CaptureFile cmdFile {};
+
+public:
+  Auto(DriveSystem *c);
+  ~Auto();
+  void SetupPlayback();
+  void ReadFile(cmd * inputs);
+  void SetupRecording();
+  void Record(cmd * inputs);
+  void CloseFile();
+ 
+
+};
+
+#endif // AUTO_H
---
+++ RAPID REACT 2022 ROBOT/2022 BOT/src/main/include/Debug.h
@@ -0,0 +1,23 @@
+//Author: Team 3229 Software Subteam
+
+#ifndef DEBUG_H
+#define DEBUG_H
+
+//This line:
+#define USE_DEBUG //Uncomment or comment to use or not use
+
+#ifdef USE_DEBUG
+#include <iostream>
+#include <frc/smartdashboard/SmartDashboard.h>
+#define debugDashSend(s,x) frc::SmartDashboard::PutData(s,x)
+#define debugDashNum(s,x) frc::SmartDashboard::PutNumber(s,x)
+#define debugCons(x) std::cout << x;
+#define debug(x) x
+#else
+#define debugDashSend(s,x)
+#define debugDashNum(s,x)
+#define debugCons(x)
+#define debug(x)
+#endif
+
+#endif // DEBUG_H
