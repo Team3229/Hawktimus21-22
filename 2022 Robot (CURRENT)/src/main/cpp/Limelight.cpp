db8cec9b7c7c0dacb38a3155dcc7a35d66d446f7
Sebastian <43454754+SebastianRPrime@users.noreply.github.com>
2022-02-27 21:30:48 -0500

    LIMELIGHT IS GETTING VALUES
    NEXT STEP IS TURRET AUTO LOCK

--- 2022 Robot (CURRENT)/src/main/cpp/Limelight.cpp
+++ 2022 Robot (CURRENT)/src/main/cpp/Limelight.cpp
@@ -10,10 +10,18 @@ Limelight::~Limelight(){
 
 void Limelight::LightOff(){
     nt::NetworkTableInstance::GetDefault().GetTable("limelight")->PutNumber("ledMode", 1);
-    debugCons("LimelighOFf\n")
+    debugCons("LimelighOFf\n");
 }
 
 void Limelight::LightOn(){
     nt::NetworkTableInstance::GetDefault().GetTable("limelight")->PutNumber("ledMode", 3);
-    debugCons("LimelightOn\n")
+    debugCons("LimelightOn\n");
+m_xOffset = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tx",0.0);         //Get horizontal off set from target
+ m_yOffset = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ty",0.0);                   //Get vertical offset from target
+ m_targetDistance = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ta",0.0);                   //Get area of target on screen
+ m_skew = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ts",0.0);                   //Get skew of target
+ m_shortDistance = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tv", 0.0);
+    debugCons( m_xOffset);
+
+
 }
--- 2022 Robot (CURRENT)/src/main/include/Limelight.h
+++ 2022 Robot (CURRENT)/src/main/include/Limelight.h
@@ -21,7 +21,11 @@ public:
     void LightOn();
    
 private:
-   
+  double m_xOffset = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tx",0.0);         //Get horizontal off set from target
+  double m_yOffset = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ty",0.0);                   //Get vertical offset from target
+  double m_targetDistance = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ta",0.0);                   //Get area of target on screen
+  double m_skew = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ts",0.0);                   //Get skew of target
+  double m_shortDistance = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tv", 0.0);
 
 };
 
